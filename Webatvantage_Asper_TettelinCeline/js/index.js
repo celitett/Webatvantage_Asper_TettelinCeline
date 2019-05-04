ac48ecefed691306a93e72683550d707e379ca3f
celitett <celine.tettelin@student.arteveldehs.be>
2019-05-04 22:01:53 +0200

    Add files via upload

---
+++ Webatvantage_Asper_TettelinCeline/js/index.js
@@ -0,0 +1,114 @@
+$(function() {
+  $('.wrapper').click(function() {
+  	$('.content').toggleClass('open');
+  	$(this).toggleClass('open');
+  })
+  
+})
+
+$(function() {
+  $('.wrapper2').click(function() {
+  	$('.content2').toggleClass('open');
+  	$(this).toggleClass('open');
+  })
+  
+})
+
+$(function() {
+  $('.wrapper3').click(function() {
+  	$('.content3').toggleClass('open');
+  	$(this).toggleClass('open');
+  })
+  
+})
+
+$(function() {
+  $('.wrapper4').click(function() {
+    $('.content4').toggleClass('open');
+    $(this).toggleClass('open');
+  })
+  
+})
+
+$(function() {
+  $('.wrapper5').click(function() {
+    $('.content5').toggleClass('open');
+    $(this).toggleClass('open');
+  })
+  
+})
+
+$(function() {
+  $('.wrapper6').click(function() {
+    $('.content6').toggleClass('open');
+    $(this).toggleClass('open');
+  })
+  
+})
+
+$(function() {
+  $('.wrapper7').click(function() {
+    $('.content7').toggleClass('open');
+    $(this).toggleClass('open');
+  })
+  
+})
+
+$(function() {
+  $('.wrapper8').click(function() {
+    $('.content8').toggleClass('open');
+    $(this).toggleClass('open');
+  })
+  
+})
+
+$(function() {
+  $('.wrapper9').click(function() {
+    $('.content9').toggleClass('open');
+    $(this).toggleClass('open');
+  })
+  
+})
+
+$(function() {
+  $('.wrapper10').click(function() {
+    $('.content10').toggleClass('open');
+    $(this).toggleClass('open');
+  })
+  
+})
+
+$(function() {
+  $('.wrapper11').click(function() {
+    $('.content11').toggleClass('open');
+    $(this).toggleClass('open');
+  })
+  
+})
+
+$(function() {
+  $('.wrapper12').click(function() {
+    $('.content12').toggleClass('open');
+    $(this).toggleClass('open');
+  })
+  
+})
+
+$(function() {
+  $('.wrapper13').click(function() {
+    $('.content13').toggleClass('open');
+    $(this).toggleClass('open');
+  })
+  
+})
+
+$(function() {
+  $('.wrapper14').click(function() {
+    $('.content14').toggleClass('open');
+    $(this).toggleClass('open');
+  })
+  
+})
+
+
+
